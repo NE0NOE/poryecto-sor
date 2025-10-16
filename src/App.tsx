9455a9c7070602c9ad3b817e8d69b06bbce54741
mendell noe parrales molina <119382034+NE0NOE@users.noreply.github.com>
2025-10-16 11:26:31 -0600

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,9 @@
-import { useState, useEffect } from 'react';
-import ChatLayout from './components/ChatLayout';
-import UsernameDialog from './components/UsernameDialog';
-import AdminPanel from './components/AdminPanel';
-import { Toaster } from './components/ui/sonner';
+import './App.css';
 
 function App() {
-  const [username, setUsername] = useState<string | null>(null);
-  const [isAdminView, setIsAdminView] = useState(false);
-
-  useEffect(() => {
-    const storedUsername = localStorage.getItem('chisChatUsername');
-    if (storedUsername) {
-      setUsername(storedUsername);
-    }
-    if (window.location.pathname === '/admin') {
-      setIsAdminView(true);
-    }
-  }, []);
-
-  const handleUsernameSet = (name: string) => {
-    localStorage.setItem('chisChatUsername', name);
-    setUsername(name);
-  };
-
-  if (isAdminView) {
-    return (
-      <>
-        <AdminPanel />
-        <Toaster />
-      </>
-    );
-  }
-
-  if (!username) {
-    return (
-      <>
-        <UsernameDialog onUsernameSet={handleUsernameSet} />
-        <Toaster />
-      </>
-    );
-  }
-
   return (
     <>
-      <ChatLayout username={username} />
-      <Toaster />
+      <div>Start prompting.</div>
     </>
   );
 }
